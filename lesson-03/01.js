6f534b04a1eae22cb55dce8f842b924fc5ba11d2
ToPraLLI <494213fa318b@mail.ru>
2025-08-20 16:25:43 +1100

    Первая задача третьего блока решена

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,9 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+
+function calculateAverage(numberOne, numberTwo) {
+	const result = (numberOne + numberTwo) / 2;
+	console.log(result);
+	
+}
+calculateAverage(2, 2)
