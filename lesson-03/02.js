cebd0b8df1d785c8c62a6fd37a574d691daae22e
ToPraLLI <494213fa318b@mail.ru>
2025-08-22 15:24:52 +1100

    Третья задача третьего блока решена

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,16 +1,8 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-
- isEven(-2)
+isEven(-2)
 
 function isEven(number) {
-
    return number === Math.floor(number / 2) * 2;
-    //return result === number;
-    // if (result === number) {
-    //     return console.log(true);        
-    // } 
-    // else {
-    //     return console.log(false);}
 }
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,7 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+findLargest(3,4,5) 
+
+function findLargest(number, numberOne, numberTwo) {
+    return Math.max(number, numberOne, numberTwo);
+}
