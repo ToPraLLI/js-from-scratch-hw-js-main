9ee0c44dacf860b8fba15664f269b31f762dd3cc
ToPraLLI <494213fa318b@mail.ru>
2025-09-01 15:57:34 +1100

    Первая задача четвертого урока решена.

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,6 +1,6 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-findLargest(3,4,5) 
+findLargest(3,4,5)
 
 function findLargest(number, numberOne, numberTwo) {
     return Math.max(number, numberOne, numberTwo);
