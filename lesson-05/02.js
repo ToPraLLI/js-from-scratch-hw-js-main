5b18717155913dcd3dc1b60421b1f4eb1d40d90b
Anton <494213fa318b@mail.ru>
2025-09-05 21:27:32 +1100

    Update 02.js

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,7 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(gallery, key, description) {
+    gallery[key] = description;
+    return gallery[key];
+}
