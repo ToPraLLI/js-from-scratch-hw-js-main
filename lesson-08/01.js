0cba39b6d468a85ac44211c2703b5cadb8dca2b4
Anton <494213fa318b@mail.ru>
2025-10-12 16:01:35 +1100

    Problem solving 8

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,22 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+    let result = '';
+    let capitalizeNext = true;
+
+    for (let i = 0; i < str.length; i++) {
+        if (capitalizeNext && str[i] !== ' ') {
+            result += str[i].toUpperCase();
+            capitalizeNext = false;
+        } else {
+            result += str[i];
+        }
+
+        if (str[i] === ' ') {
+            capitalizeNext = true;
+        }
+    }
+
+    return result;
+}
--- lesson-08/02.js
+++ lesson-08/02.js
@@ -4,7 +4,7 @@
  */
 
 function isNumeric(str) {
-  // your code
+    return !isNaN(parseFloat(str)) && isFinite(str) && str.trim() !== '';
 }
 
 // console.log(isNumeric("123")) // Ожидаемый результат: true
