df24f6f27b374b159c17961417452ba2915137e1
Anton <494213fa318b@mail.ru>
2025-10-12 16:18:08 +1100

    Problem solving 8

--- lesson-08/03.js
+++ lesson-08/03.js
@@ -10,6 +10,8 @@
 */
 
 function truncate(str, maxLength) {
-    if (str.length <= maxLength) return str;
-    return str.substring(0, maxLength - 1) + '…';
+  if (str.length > maxLength) {
+    return str.slice(0, maxLength) + '...';
+  }
+  return str;
 }
